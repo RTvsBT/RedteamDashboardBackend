bc416daa8bc7c09fff965a58ff136cf7abcc312c
testerzs <djibrahim@live.nl>
2020-11-26 12:25:44 +0100

    Update Hello_world.py

--- Hello_world.py
+++ Hello_world.py
@@ -1,15 +1,9 @@
 import flask, subprocess
 from flask import request, jsonify
  
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-
-#Home
-# @app.route('/', methods=['GET'])
-# def home():
-#     return '''<h1>Cyber Attacks</h1>
-# <p>A prototype API for cyber attackzz.</p>'''
-
 
 @app.route('/', methods=['GET'])
 def home():
@@ -28,6 +22,7 @@ def page_not_found(e):
     return jsonify({'error': 'Not found 404'}), 404
 
 
+# Return the possible atacks 
 @app.route('/api/v1/resources/attacks/all', methods=['GET'])
 def api_all():
     cmd = ["python3","main.py", "-l"]
@@ -38,4 +33,16 @@ def api_all():
     out = out.decode()
     return jsonify(command="list", consoleCommand='python3 main.py -l', output=out)
 
+# Runs GetHostname attack
+@app.route('/api/v1/resources/attacks/gethostname', methods=['GET'])
+def api_gethostname():
+    cmd = ["python3","main.py", "-p", "GetHostname"]
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            stdin=subprocess.PIPE)
+    out,err = p.communicate()
+    out = out.decode()
+    return jsonify(command="gethostname", consoleCommand='python3 main.py -p GetHostname', output=out)
+
+    
 app.run()
