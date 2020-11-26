cf7f28bc2db1a0d312f9b6ddea9ba839c379e0a0
testerzs <djibrahim@live.nl>
2020-11-26 11:27:33 +0100

    Update Hello_world.py

--- Hello_world.py
+++ Hello_world.py
@@ -1,21 +1,8 @@
 import flask, subprocess
 from flask import request, jsonify
  
-
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-
-# Create some test data for our catalog in the form of a list of dictionaries.
-attacks = [
-    {'id': 0,
-     'title': 'NMAP Scan',
-     'response': 'Veel outpout'},
-    {'id': 1,
-     'title': 'Buffer overflow',
-     'response': 'You got root access '},
-    {'id': 2,
-     'response': '1337 hack'}
-]
 
 #Home
 # @app.route('/', methods=['GET'])
@@ -26,14 +13,14 @@ attacks = [
 
 @app.route('/', methods=['GET'])
 def home():
-    cmd = ["python","main.py", "-d"]
+    cmd = ["python3","main.py", "-l"]
     p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
                             stderr=subprocess.PIPE,
                             stdin=subprocess.PIPE)
     out,err = p.communicate()
-    print (out)
-    return out
-
+    out = out.decode()
+    print(out) 
+    return jsonify(command="list", output=out)
 
 #Error 
 @app.errorhandler(404)
@@ -41,32 +28,14 @@ def page_not_found(e):
     return jsonify({'error': 'Not found 404'}), 404
 
 
-# A route to return all of the available entries in our catalog.
 @app.route('/api/v1/resources/attacks/all', methods=['GET'])
 def api_all():
-    return jsonify(attacks)
-
-@app.route('/api/v1/resources/attacks', methods=['GET'])
-def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
-
-    # Create an empty list for our results
-    results = []
-
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
-    for attack in attacks:
-        if attack['id'] == id:
-            results.append(attack)
-
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
-    return jsonify(results)
+    cmd = ["python3","main.py", "-l"]
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            stdin=subprocess.PIPE)
+    out,err = p.communicate()
+    out = out.decode()
+    return jsonify(command="list", consoleCommand='python3 main.py -l', output=out)
 
 app.run()
