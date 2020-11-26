9a0c7c15dc1c13a4c577fe18f6948ccb9e9b311b
testerzs <djibrahim@live.nl>
2020-11-26 12:40:21 +0100

    Update Hello_world.py

--- Hello_world.py
+++ Hello_world.py
@@ -5,44 +5,35 @@ from flask import request, jsonify
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/', methods=['GET'])
-def home():
-    cmd = ["python3","main.py", "-d"]
+def command_runner(cmd):
     p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
                             stderr=subprocess.PIPE,
                             stdin=subprocess.PIPE)
     out,err = p.communicate()
     out = out.decode()
-    print(out) 
-    return jsonify(command="list", output=out)
+    return out
+
+@app.route('/', methods=['GET'])
+def home():
+    cmd = ["python3","main.py", "-d"]
+    print(command_runner(cmd)) 
+    return jsonify(command="list", output=command_runner(cmd))
+
+# Return the possible atacks 
+@app.route('/api/v1/resources/attacks/all', methods=['GET'])
+def api_all():
+    cmd = ["python3","main.py", "-l"]
+    return jsonify(command="list", consoleCommand='python3 main.py -l', output=command_runner(cmd))
+
+# Runs GetHostname attack
+@app.route('/api/v1/resources/attacks/gethostname', methods=['GET'])
+def api_gethostname():
+    cmd = ["python3","main.py", "-p", "GetHostname"]
+    return jsonify(command="gethostname", consoleCommand='python3 main.py -p GetHostname',  output=command_runner(cmd))
 
 #Error 
 @app.errorhandler(404)
 def page_not_found(e):
     return jsonify({'error': 'Not found 404'}), 404
 
-
-# Return the possible atacks 
-@app.route('/api/v1/resources/attacks/all', methods=['GET'])
-def api_all():
-    cmd = ["python3","main.py", "-l"]
-    p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            stdin=subprocess.PIPE)
-    out,err = p.communicate()
-    out = out.decode()
-    return jsonify(command="list", consoleCommand='python3 main.py -l', output=out)
-
-# Runs GetHostname attack
-@app.route('/api/v1/resources/attacks/gethostname', methods=['GET'])
-def api_gethostname():
-    cmd = ["python3","main.py", "-p", "GetHostname"]
-    p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            stdin=subprocess.PIPE)
-    out,err = p.communicate()
-    out = out.decode()
-    return jsonify(command="gethostname", consoleCommand='python3 main.py -p GetHostname', output=out)
-
-    
 app.run()
