ca2775c2ac407333da997084dfd418cf0219c7d4
testerzs <djibrahim@live.nl>
2020-11-27 20:57:45 +0100

    Update Hello_world.py
    ADD: Cors and NIDS / HIDS mocks

--- Hello_world.py
+++ Hello_world.py
@@ -1,9 +1,11 @@
-import flask, subprocess
-from flask import request, jsonify
+import flask, subprocess, markupsafe 
+from flask import request, jsonify, escape
+from flask_cors import CORS
  
-
 app = flask.Flask(__name__)
+CORS(app)
 app.config["DEBUG"] = True
+
 
 def command_runner(cmd):
     p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
@@ -19,7 +21,7 @@ def home():
     print(command_runner(cmd)) 
     return jsonify(command="list", output=command_runner(cmd))
 
-# Return the possible atacks 
+# Return the possible attacks 
 @app.route('/api/v1/resources/attacks/all', methods=['GET'])
 def api_all():
     cmd = ["python3","main.py", "-l"]
@@ -31,9 +33,63 @@ def api_gethostname():
     cmd = ["python3","main.py", "-p", "GetHostname"]
     return jsonify(command="gethostname", consoleCommand='python3 main.py -p GetHostname',  output=command_runner(cmd))
 
+
+# With parameter
+
+# NIDS
+# NIDS_Mock1
+@app.route('/api/v1/resources/attacks/NIDS_Mock1/<host>')
+def api_nids1_attack(host):
+    # return 'User %s' % escape(username)
+    cmd = ["python3","main.py", "-p", "NIDS_Mock1", "--host", host]
+    print(cmd)
+    return jsonify(command="NIDS_Mock1", consoleCommand='python3 main.py -p NIDS_Mock1 --host 192.168.XXX.XXX',  output=command_runner(cmd))
+
+# NIDS_Mock2
+@app.route('/api/v1/resources/attacks/NIDS_Mock2/<host>')
+def api_nids2_attack(host):
+    # return 'User %s' % escape(username)
+    cmd = ["python3","main.py", "-p", "NIDS_Mock2", "--host", host]
+    print(cmd)
+    return jsonify(command="NIDS_Mock2", consoleCommand='python3 main.py -p NIDS_Mock2 --host 192.168.XXX.XXX',  output=command_runner(cmd))
+
+# NIDS_Mock3
+@app.route('/api/v1/resources/attacks/NIDS_Mock3/<host>')
+def api_nids3_attack(host):
+    # return 'User %s' % escape(username)
+    cmd = ["python3","main.py", "-p", "NIDS_Mock3", "--host", host]
+    print(cmd)
+    return jsonify(command="NIDS_Mock1", consoleCommand='python3 main.py -p NIDS_Mock3 --host 192.168.XXX.XXX',  output=command_runner(cmd))
+
+
+# HIDS
+# HIDS_Mock1
+@app.route('/api/v1/resources/attacks/HIDS_Mock1/<host>')
+def api_hids1_attack(host):
+    # return 'User %s' % escape(username)
+    cmd = ["python3","main.py", "-p", "HIDS_Mock1", "--host", host]
+    print(cmd)
+    return jsonify(command="HIDS_Mock1", consoleCommand='python3 main.py -p HIDS_Mock1 --host 192.168.XXX.XXX',  output=command_runner(cmd))
+
+# HIDS_Mock2
+@app.route('/api/v1/resources/attacks/HIDS_Mock2/<host>')
+def api_hids2_attack(host):
+    # return 'User %s' % escape(username)
+    cmd = ["python3","main.py", "-p", "HIDS_Mock2", "--host", host]
+    print(cmd)
+    return jsonify(command="HIDS_Mock2", consoleCommand='python3 main.py -p HIDS_Mock2 --host 192.168.XXX.XXX',  output=command_runner(cmd))
+
+# HIDS_Mock3
+@app.route('/api/v1/resources/attacks/HIDS_Mock3/<host>')
+def api_hids3_attack(host):
+    # return 'User %s' % escape(username)
+    cmd = ["python3","main.py", "-p", "HIDS_Mock3", "--host", host]
+    print(cmd)
+    return jsonify(command="HIDS_Mock3", consoleCommand='python3 main.py -p HIDS_Mock3 --host 192.168.XXX.XXX',  output=command_runner(cmd))
+
 #Error 
 @app.errorhandler(404)
 def page_not_found(e):
     return jsonify({'error': 'Not found 404'}), 404
 
-app.run(host='0.0.0.0')
+app.run()
